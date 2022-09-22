915fa4ee1b5d984aec2ebf6b4241730e14efbfd2
wonhee <asdkcone13@gmail.com>
2022-09-22 21:56:39 +0900

    GoodsInfo finished

--- GoodsInfo.java
+++ GoodsInfo.java
@@ -1,44 +1,36 @@
-//* 다음 조건을 맞춰 상품정보 클래스 선언하기
-//3. 상품정보(GoodsInfo) 클래스 선언하기
-//	필드 : 상품명, 상품코드, 제조사, 표준단가, 할인율(ex_10%)
-//	생성자 : 할인율을 제외한 필드를 모두 초기화하는 생성자
-//			 모든 필드를 초기화하는 생성자(할인율이 없는 경우 추가)
-//	메소드 : 할인율을 변경하는 메소드(입력한 값 만큼 변경)
-//			 판매가를 계산하고 반환하는 메소드(판매가 = 표준단가에 할인율 적용) 
-//			 
-//4. 3의 클래스를 사용하는 main이 있는 클래스(GoodsInfoMain)선언
-//
-//	[출력 예시]
-//	제조사		상품코드	상품명		표준단가	할인율		판매가
-//	NB			nb01		운동화		70000		0			70000
-//	NIKE		nk01		모자		30000		30			21000
-//	
+import java.text.DecimalFormat;
+
 public class GoodsInfo {
 	
-	String company, goodsCode, name;
-	int standardPrice, price, sale;
+	String company, name;
+	int standardPrice, sale, goodsCode;
+	double price;
 	
-	GoodsInfo(String company, String goodsCode, String name, int standardPrice, int price){
+	GoodsInfo(String company, int goodsCode, String name, int standardPrice){
 		this.company = company;
 		this.goodsCode = goodsCode;
 		this.name = name;
 		this.standardPrice = standardPrice;
-		this.price = price;
 	}
-	GoodsInfo(String company, String goodsCode, String name, int standardPrice, int price, int sale){
-		this(company, goodsCode, name, standardPrice, price);
+	GoodsInfo(String company, int goodsCode, String name, int standardPrice, int sale){
+		this(company, goodsCode, name, standardPrice);
 		this.sale = sale;
 	}
 	
 	//할인율 변경/반환 메소드
-	int getSale(int inputSale) {
+	void setSale(int inputSale) {
 		sale = inputSale;
-		return sale;
 	} //getSale()
 	
-	int getPrice() {
-		return price * ((100 - sale)/100);
+	String getPrice() {
+		DecimalFormat df = new DecimalFormat("0");
+		if(sale==0) {
+			this.price = standardPrice;
+		}else {
+			this.price = standardPrice * ((100 - (double)sale)/100);
+//			System.out.println("★"+price+"★"+standardPrice);
+		}
+		return df.format(price);
 	} //getPrice()
-	
 	
 } //class
--- GoodsInfoMain.java
+++ GoodsInfoMain.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class GoodsInfoMain {
 	public static void main(String[] args) {
-		GoodsInfo p1 = new GoodsInfo("아디다스", "AD01", "운동화", 70000, 0 );
+		Scanner sc = new Scanner(System.in);
+		GoodsInfo p1 = new GoodsInfo("아디다스", 1, "운동화", 70000);
+		display(p1);
+		GoodsInfo p2 = new GoodsInfo("쿠크다스", 2, "런닝화", 30000, 30);
+		display(p2);
+		
+		System.out.print("할인율 변경(상품 코드 입력) :");
+		int inputStart = Integer.parseInt(sc.nextLine());
+		System.out.print("할인율(%) 설정 :");
+		int inputSale = Integer.parseInt(sc.nextLine());
+		System.out.println();
+		
+		if(inputStart == 1) {
+			p1.setSale(inputSale);
+			display(p1);
+		}else if(inputStart == 2) {
+			p2.setSale(inputSale);
+			display(p2);
+		}
+		
+		sc.close();
+		
 	} //main()
+	
+	static void display(GoodsInfo info) {
+		System.out.println("제조사\t\t상품코드\t상품명");
+		System.out.print(info.company+"\t"+info.goodsCode+"\t\t"+info.name+"\n\n");
+		System.out.println("표준단가\t할인율\t\t판매가");
+		System.out.print(info.standardPrice+"\t\t"+info.sale+"\t\t"+info.getPrice()+"\n");
+		System.out.println("======================================\n");
+	} //display()
+	
 } //class
